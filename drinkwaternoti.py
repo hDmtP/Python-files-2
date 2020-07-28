e466acc9ab8f5087658705a72a7200c5eafa9c59
Pritam Dhara <pritamdhara914@gmail.com>
2020-07-28 15:29:53 +0530

    version2

--- drinkwaternoti.py
+++ drinkwaternoti.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
         notification.notify(
             title="**Please Drink WATER Now!!",
             message="The National Academies of Sciences, Engineering, and Medicine determined that an adequate daily fluid intake is: About 15.5 cups (3.7 liters) of fluids for men.",
-            app_icon=r"C:\Users\user\Downloads\water.ico",
+            app_icon=r"C:\Users\user\Downloads\Images\water.ico",
             timeout=12
         )
         time.sleep(3600)
